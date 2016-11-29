03d9c399806b8b6b04c5e07f11431f19c16fca76
DeBusscherePieter <pjdbussc@gmail.com>
2016-11-29 18:55:32 +0100

    Try 1 voor een mogelijks werkende versie

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var helpers = require('handlebars-helpers')();
+var index = require('./routes/index');
+var collections = ["reader","efpluser"];
+var mongojs = require('mongojs');
+var dbjs = mongojs('mongodb://admin:admin@ds013619.mlab.com:13619/hogentresto',collections);
+var app = express();
+
+var cart = [];
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+
+app.use(function(req,res,next){
+  var _send = res.send;
+  var sent = false;
+  res.send = function(data){
+    if(sent) return;
+    _send.bind(res)(data);
+    sent = true;
+};
+  next();
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var InfiniteLoop = require('infinite-loop');
+var il = new InfiniteLoop();
+/* GET home page. */
+
+
+router.get('/', function(req, res, next) {
+  dbjs.reader.remove({}, function(err,doc){
+    console.log(doc);
+  });
+  res.render('index', {title:'Express'});
+});
+
+
+var collections = ["reader", "efpl", "efpluser"];
+var mongojs = require('mongojs');
+var dbjs = mongojs('mongodb://admin:admin@ds013619.mlab.com:13619/hogentresto',collections);
+var app = express();
+
+var cart = [];
+
+router.get('/reader/:id', function(req,res){
+  dbjs.reader.insert({'id': req.params.id});
+});
+
+router.post('/addcart', function(req,res){
+  cart.push({title: req.body.title, mealname: req.body.mealname, price: req.body.price, date: req.body.date});
+  console.log(cart[0].title);
+});
+
+router.get('/refresh', function(req,res){
+  dbjs.reader.findOne({},function (err, doc) {
+    if (!doc) {
+        // we visited all docs in the collection
+        res.redirect('/');
+    } else {
+        res.redirect('/kassa/' + doc.id);
+    }
+
+});
+});
+
+router.post('/efpl', function(req, res){
+  for(i=0; i < cart.length; i++){
+    dbjs.efpl.insert({'user' : req.body.name, 'meal': cart[i].mealname, 'title': cart[i].title, 'price': cart[i].price, 'date': cart[i].date});
+  }
+  dbjs.reader.remove({}, function(err,doc){
+    console.log(doc);
+  });
+  res.redirect('/');
+});
+
+router.get('/kassa/:id', function(req, res, next){
+  cart = [];
+  dbjs.efpluser.findOne({'id' : req.params.id},function (err, doc) {
+    if (!doc) {
+        // we visited all docs in the collection
+    } else {
+        res.render('kassa', {id: req.params.id, name: doc.name});
+    }
+
+  });
+});
+
+module.exports = router;
